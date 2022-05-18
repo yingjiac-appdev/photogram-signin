18435a9b30e78c19e255065ed9958dbe4626c43f
yingjiac <yingjiac@uchicago.edu>
2022-05-18 20:33:07 +0000

    halfway...

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,25 @@
 class UsersController < ApplicationController
+  def authenticate
+    
+    render({ :plain => "hi"})
+  end
+  
+  def toast_cookies
+    reset_session
+
+    redirect_to("/", { :notice => "See ya later!"})
+  end  
+
+  def new_registration_form
+
+    render({ :template => "users/signup_form.html.erb"})
+  end   
+
+  def new_session_form
+
+    render({ :template => "users/signin_form.html.erb"})
+  end 
+
   def index
     @users = User.all.order({ :username => :asc })
 
@@ -16,10 +37,19 @@ class UsersController < ApplicationController
     user = User.new
 
     user.username = params.fetch("input_username")
+    user.password = params.fetch("input_password")
+    user.password_confirmation = params.fetch("input_password_confirmation")
 
-    user.save
+    save_status = user.save
 
-    redirect_to("/users/#{user.username}")
+    if save_status == true
+      session.store(:user_id, user.id)
+
+      redirect_to("/users/#{user.username}", { :notice => "Welcome, " + user.username + "!"})
+    else
+      redirect_to("/user_sign_up", { :alert => user.errors.full_messages.to_sentence})
+    end
+
   end
 
   def update
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,14 @@ Rails.application.routes.draw do
 
   # User routes
 
+  get("/user_sign_up", { :controller => "users", :action => "new_registration_form"})
+
+  get("/user_sign_out", { :controller => "users", :action => "toast_cookies"})
+
+  get("/user_sign_in", { :controller => "users", :action => "new_session_form"})
+
+  post("/verify_credentials", { :controller => "users", :action => "authenticate"})
+
   # CREATE
   get("/insert_user_record", {:controller => "users", :action => "create" })
 
